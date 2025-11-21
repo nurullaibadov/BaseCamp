36d7b2cd94263a2e5fb7dc9dd7943e16533608b2
nurullaibadov <ibadnurulla@gmail.com>
2025-11-21 09:43:12 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+const express = require('express');
+const path = require('path');       
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { 
+    title: 'My Awesome Express + EJS App',
+    message: 'Hello from EJS!'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
